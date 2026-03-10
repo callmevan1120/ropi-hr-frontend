0f75a960e19e08117a291b5fe144ed3f8d1d4ba3
Vany Eka <vanyekak@gmail.com>
2026-03-10 08:15:36 +0700

    test: isolasi ketat hanya halaman login

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,18 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-// Import semua halaman yang SUDAH kita perbaiki
+// KITA CUMA IMPORT LOGIN
 import Login from './pages/Login';
-import Home from './pages/Home';
-import Absen from './pages/Absen';
-import Cuti from './pages/Cuti';
-import Profil from './pages/Profil';
-import Izin from './pages/Izin';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Rute Penunjuk Jalan (Penting biar ga layar putih pas buka web awal) */}
+        {/* Rute Penunjuk Jalan */}
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="*" element={<Navigate to="/login" replace />} />
 
-        {/* Rute Utama Aplikasi */}
+        {/* HANYA HALAMAN INI YANG HIDUP */}
         <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/absen" element={<Absen />} />
-        <Route path="/cuti" element={<Cuti />} />
-        <Route path="/profil" element={<Profil />} />
-        <Route path="/izin" element={<Izin />} />
       </Routes>
     </BrowserRouter>
   );
